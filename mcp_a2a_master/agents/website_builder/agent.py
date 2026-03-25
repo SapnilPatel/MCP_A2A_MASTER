4e5e780d1cd2d2192699eb79f5eb586083e7bef2
Sapnil <sapnilpatel1482@gmail.com>
2026-03-24 18:03:39 -0700

    Completed agent_executor.py

--- mcp_a2a_master/agents/website_builder/agent.py
+++ mcp_a2a_master/agents/website_builder/agent.py
@@ -1,5 +1,12 @@
-from utilities.common.file_loader import load_instructions_file # type: ignore
-from google.adk.agent import LlmAgent # type: ignore
+from typing import AsyncIterable
+from google.adk import Runner # type: ignore
+from google.adk.agents import LlmAgent
+from requests import session  # type: ignore
+from utilities.common.file_loader import load_instructions_file 
+from google.adk.artifacts import InMemoryArtifactService 
+from google.adk.sessions import InMemorySessionService
+from google.adk.memory import InMemoryMemoryService
+from google.genai import types 
 
 class WebsiteBuilder:
     """
@@ -10,6 +17,15 @@ class WebsiteBuilder:
     def __init__(self):
         self.system_instruction = load_instructions_file("agents/website_builder/instructions.txt")
         self.description = load_instructions_file("agents/website_builder/description.txt")
+        self._agent = self._build_agent()
+        self._user_id = "website_builder_user"
+        self.runner = Runner(
+            app_name = self._agent_name,
+            agent = self._agent,
+            artifact_service=InMemoryArtifactService(),
+            session_service=InMemorySessionService(),
+            memory_service=InMemoryMemoryService()
+        )
 
     def _build_agent(self)->LlmAgent:
         return LlmAgent(
@@ -18,3 +34,52 @@ class WebsiteBuilder:
             instruction=self.system_instruction,
             description=self.description,
         )
+    
+    async def invoke(self, query: str, session_id: str)-> AsyncIterable[dict]:
+        """
+        Invoke the agent 
+        Return a stream of updates back to the caller as agent process the query
+
+        {
+            'is_task_complete': bool, # indicates if the agent has completed processing the query
+            'updates': str, # intermediate updates from the agent while processing the query
+            'content': str # final content/result from the agent after processing the query
+        }
+        """
+        session = await self._runner.session_service.get_session(
+            app_name=self._agent.name,
+            session_id=session_id,
+            user_id=self._user_id    
+        )
+
+        if not session:
+            session = await self._runner.session_service.create_session(
+                app_name=self._agent.name,
+                session_id=session_id,
+                user_id=self._user_id
+            )
+        
+        user_content = types.Content(
+            role = "user",
+            parts = [types.Part.from_text(text=query)]
+        )
+
+        async for event in self._runner.run_async(
+            user_id = self._user_id,
+            session_id = session_id,
+            new_messages = user_content
+        ):
+            if event.is_final_response:
+                final_response = ""
+                if event.contgent and event.content.parts and event.content.parts[-1].text:
+                    final_response = event.content.parts[-1].text
+
+                yield{
+                    'is_task_complete': True,
+                    'content': final_response
+                }
+            else:
+                yield {
+                    'is_task_complete': False,
+                    'updates': event.content.parts[-1].text
+                }
--- mcp_a2a_master/agents/website_builder/agent_executor.py
+++ mcp_a2a_master/agents/website_builder/agent_executor.py
@@ -1,9 +1,17 @@
+import asyncio
+from a2a.utils.errors import ServerError
 from a2a.server.events import EventQueue # type: ignore
 from a2a.server.tasks import TaskUpdater # type: ignore
 from agents.website_builder.agent import WebsiteBuilder # type: ignore
 from a2a.server.agent_execution import AgentExecutor, RequestContext 
 from a2a.utils import (
-    new_task
+    new_task,
+    new_agent_text_message
+)
+from a2a.types import (
+    Task,
+    TaskState,
+    UnsupportedOperationError
 )
 
 class WebsiteBuilderAgentExecutor(AgentExecutor):
@@ -23,6 +31,37 @@ class WebsiteBuilderAgentExecutor(AgentExecutor):
             task = new_task(context.message)
             await event_quque.enqueue_event(task)
 
-        updater = TaskUpdater(event_quque, task.id, task.contextId)
+        self.agent.invoke(query, task.context_id)
+        updater = TaskUpdater(event_quque, task.id, task.context_id)
 
-        
+        try:
+            async for item in self.agent.invoke(query, task.context_id):
+                is_task_complete = item.get("is_task_complete", False)
+
+                if not is_task_complete:
+                    message = item.get('updates', 'The agent is working on your request.')
+                    await updater.update_status(
+                        TaskState.working,
+                        new_agent_text_message(message, task.context_id, task.id)
+                    )
+                else:
+                    final_result = item.get('content', 'No result received')
+                    await updater.update_status(
+                        TaskState.completed,
+                        new_agent_text_message(final_result, task.context_id, task.id)
+                    )
+                    await asyncio.sleep(0.1)  # Allow time for the message to be processed
+
+                    break
+        except Exception as e:
+            error_message = f"An error occurred while executing the agent: {str(e)}"
+
+            await updater.update_status(
+                TaskState.failed,
+                new_agent_text_message(error_message, task.context_id, task.id)
+            )
+    
+    async def cancel(self, request: RequestContext, event_queue: EventQueue) -> Task | None:
+        raise ServerError(error=UnsupportedOperationError())
+    
+    
